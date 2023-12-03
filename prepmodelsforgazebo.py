42b00204efe465351e8d6862ffc5c04f7e0f9376
rms <rms>
2023-12-03 19:35:41 +0000

    added file to export blender objects to a folder that for importing to gazebo

---
+++ prepmodelsforgazebo.py
@@ -0,0 +1,84 @@
+# prep models in .blend to folder in gazebo database
+parts_folder = "/home/rms20/Github/autoassembly/parts/biplane/"
+database_path = "/home/rms/Github/autoassembly/parts/database/"
+base_blend_file = "/home/rms/Github/autoassembly/base.blend"
+
+# loop objects in .blend folder
+import os
+import bpy
+
+for file in os.listdir(parts_folder):
+    if file[-4:] != '.STL':
+        continue
+    full_file = parts_folder + file
+    bpy.ops.import_mesh.stl(filepath=full_file)
+
+bpy.ops.mesh.select_all(action = 'DESELECT')
+
+# centre objects
+for obj in bpy.data.objects:
+    obj.location.x = 0
+    obj.location.y = 0
+    obj.location.z = 0
+
+# duplicate files
+
+def model_config(file, name, description):
+    with open(file, 'w') as f:
+        f.write(f"""<?xml version="1.0"?>
+<model>
+  <name>%s</name>
+  <version>1.0</version>
+  <sdf version="1.6">model.sdf</sdf>
+  <author>
+    <name>Robert Smith</name>
+    <email></email>
+  </author>
+  <description>
+    %s
+  </description>
+</model>
+""" % (name, description))
+
+def model_sdf(file, name, collision, visual):
+    with open(file, 'w') as f:
+        f.write(f"""<?xml version="1.0" ?>
+<sdf version="1.6">
+  <model name="%s">
+    <static>true</static>
+    <link name="link">
+      <collision name="collision">
+        <geometry>
+          <mesh>
+            <scale>0.01 0.01 0.01</scale>
+            <uri>model://%s</uri>
+          </mesh>
+        </geometry>
+      </collision>
+      <visual name="visual">
+        <geometry>
+          <mesh>
+            <scale>0.01 0.01 0.01</scale>
+            <uri>model://%s</uri>
+          </mesh>
+        </geometry>
+      </visual>
+    </link>
+  </model>
+</sdf>
+""" % (name, collision, visual))
+
+# export all objects
+for obj in bpy.data.objects:
+    new_folder = database_path + obj.name + "/"
+    stl_path = new_folder + obj.name + '.stl'
+    dae_path = new_folder + obj.name + '.dae'
+    os.mkdir(new_folder)
+    obj.select_set(True)
+    model_config(new_folder + "model.config", obj.name, obj.name)
+    collision = f"{obj.name}/{obj.name}.stl"
+    visual = f"{obj.name}/{obj.name}.dae"
+    model_sdf(new_folder + "model.sdf", obj.name, collision, visual)
+    bpy.ops.export_mesh.stl(filepath=stl_path, use_selection=True)
+    bpy.ops.wm.collada_export(filepath=dae_path, selected=True)
+    obj.select_set(False)
